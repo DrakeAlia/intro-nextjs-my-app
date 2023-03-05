f8f87b0b1975e3db608d9bef0cbd58579fef6812
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2023-03-04 17:11:40 -0800

    first commit, deleted previous repo of this version

---
+++ app/page.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import styles from "../styles/home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.home}>
+      <div>
+        <div>
+          <h1>Hi, My name is Drake!!!</h1>
+        </div>
+        <div>
+          <Link href="/blog">Checkout my Blog</Link>
+        </div>
+        <div>
+          <Link href="/contact">Contact Me</Link>
+        </div>
+      </div>
+    </div>
+  );
+}
